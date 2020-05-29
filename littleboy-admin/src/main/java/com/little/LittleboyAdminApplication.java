af7fce5d517f596fd34a3171fbd7879e462eea9f
liyuan <ErichLee@qq.com>
2020-05-29 15:10:39 +0800

    Initial commit

---
+++ littleboy-admin/src/main/java/com/little/LittleboyAdminApplication.java
@@ -0,0 +1,13 @@
+package com.little;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LittleboyAdminApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(LittleboyAdminApplication.class, args);
+  }
+
+}
